22bc750587e83f92a58d13fcffe3010696bdf1a7
nanikit <kitnani@yandex.com>
2020-12-05 04:57:01 +0900

    refactor!: unify initialization function

--- src/mod.ts
+++ src/mod.ts
@@ -3,7 +3,7 @@ import { ComicSource, ViewerSource } from './types.ts';
 import { timeout } from './utils.ts';
 import { initializeViewer } from './viewer.tsx';
 
-export const initializeWithSource = async (source: ComicSource) => {
+const initializeWithSource = async (source: ComicSource) => {
   const root = document.createElement('div');
   while (true) {
     if (document.body) {
@@ -15,10 +15,14 @@ export const initializeWithSource = async (source: ComicSource) => {
   }
 };
 
-export const initialize = async (sources: ViewerSource[]) => {
-  const source = sources.find((x) => x.isApplicable());
-  if (source) {
-    await initializeWithSource(source.comicSource);
+export const initialize = async (sourceOrSources: ViewerSource | ViewerSource[]) => {
+  if (Array.isArray(sourceOrSources)) {
+    const source = sourceOrSources.find((x) => x.isApplicable());
+    if (source) {
+      await initializeWithSource(source.comicSource);
+    }
+  } else {
+    await initializeWithSource(sourceOrSources.comicSource);
   }
 };
 
