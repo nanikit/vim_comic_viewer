3639b4291559f1ae986aef2098ef72c6805f82db
nanikit <kitnani@yandex.com>
2020-12-05 10:43:22 +0900

    refactor: extract function

--- denopack_config.ts
+++ denopack_config.ts
@@ -4,6 +4,33 @@ import {
   useCache,
 } from 'https://deno.land/x/denopack@0.10.0/mod.ts';
 
+const denoFmt = async (code: string) => {
+  const process = Deno.run({
+    cmd: ['deno', 'fmt', '-'],
+    stdin: 'piped',
+    stdout: 'piped',
+  });
+  const input = new TextEncoder().encode(code);
+  await process.stdin.write(input);
+  process.stdin.close();
+
+  const formatteds = [];
+  const accumulates = [0];
+  let sum = 0;
+  for await (const chunk of Deno.iter(process.stdout)) {
+    formatteds.push(new Uint8Array(chunk));
+    sum += chunk.length;
+    accumulates.push(sum);
+  }
+
+  const concatenated = new Uint8Array(sum);
+  for (let i = 0; i < formatteds.length; i++) {
+    concatenated.set(formatteds[i], accumulates[i]);
+  }
+  const decoded = new TextDecoder().decode(concatenated);
+  return decoded;
+};
+
 const json = Deno.readTextFileSync('./tsconfig.json');
 const compilerOptions = JSON.parse(json).compilerOptions;
 
@@ -32,30 +59,7 @@ const postprocessPlugin = {
         continue;
       }
 
-      const process = Deno.run({
-        cmd: ['deno', 'fmt', '-'],
-        stdin: 'piped',
-        stdout: 'piped',
-      });
-      const input = new TextEncoder().encode(output.code);
-      await process.stdin.write(input);
-      process.stdin.close();
-
-      const formatteds = [];
-      const accumulates = [0];
-      let sum = 0;
-      for await (const chunk of Deno.iter(process.stdout)) {
-        formatteds.push(new Uint8Array(chunk));
-        sum += chunk.length;
-        accumulates.push(sum);
-      }
-
-      const concatenated = new Uint8Array(sum);
-      for (let i = 0; i < formatteds.length; i++) {
-        concatenated.set(formatteds[i], accumulates[i]);
-      }
-      const decoded = new TextDecoder().decode(concatenated);
-      output.code = decoded;
+      output.code = await denoFmt(output.code);
     }
   },
 };
