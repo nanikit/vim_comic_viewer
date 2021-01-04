5981d51d1f29b289e13b4f32a9da384571fb1aea
nanikit <kitnani@yandex.com>
2021-01-05 01:36:55 +0900

    feat: remove user agent default border

--- src/components/scrollable_layout.ts
+++ src/components/scrollable_layout.ts
@@ -1,6 +1,8 @@
 import { styled } from '../vendors/stitches.ts';
 
 export const ScrollableLayout = styled('div', {
+  // chrome user-agent style override
+  outline: 0,
   position: 'relative',
   backgroundColor: '#eee',
   height: '100%',
