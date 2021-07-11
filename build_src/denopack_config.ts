b6c9514c1b94d477d2ba5df43f5395d861973b56
nanikit <nanikit4@gmail.com>
2021-07-12 02:28:07 +0900

    docs: add localized info

--- build_src/denopack_config.ts
+++ build_src/denopack_config.ts
@@ -4,19 +4,21 @@ const postprocessPlugin: Plugin = {
   name: "postprocess-plugin",
 
   banner: `// ==UserScript==
-// @name         vim comic viewer
-// @description  Universal comic reader
-// @version      6.2.0
-// @namespace    https://greasyfork.org/en/users/713014-nanikit
-// @exclude      *
-// @match        http://unused-field.space/
-// @author       nanikit
-// @license      MIT
-// @resource     fflate           https://cdn.jsdelivr.net/npm/fflate@0.7.1/lib/browser.cjs
-// @resource     react            https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.production.min.js
-// @resource     react-dom        https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.production.min.js
-// @resource     @stitches/core   https://cdn.jsdelivr.net/npm/@stitches/core@0.2.0/dist/index.cjs
-// @resource     @stitches/react  https://cdn.jsdelivr.net/npm/@stitches/react@0.2.0/dist/index.cjs
+// @name           vim comic viewer
+// @description    Universal comic reader
+// @name:ko        vim comic viewer
+// @description:ko 만화 뷰어 라이브러리
+// @version        6.2.0
+// @namespace      https://greasyfork.org/en/users/713014-nanikit
+// @exclude        *
+// @match          http://unused-field.space/
+// @author         nanikit
+// @license        MIT
+// @resource       fflate           https://cdn.jsdelivr.net/npm/fflate@0.7.1/lib/browser.cjs
+// @resource       react            https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.production.min.js
+// @resource       react-dom        https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.production.min.js
+// @resource       @stitches/core   https://cdn.jsdelivr.net/npm/@stitches/core@0.2.0/dist/index.cjs
+// @resource       @stitches/react  https://cdn.jsdelivr.net/npm/@stitches/react@0.2.0/dist/index.cjs
 // ==/UserScript==
 `,
 
