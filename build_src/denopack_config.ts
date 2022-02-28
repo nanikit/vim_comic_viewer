8ec3e76c22ee3beb761c7fea05b450ab4dce6ded
nanikit <nanikit4@gmail.com>
2022-03-01 06:59:21 +0900

    fix: version

--- build_src/denopack_config.ts
+++ build_src/denopack_config.ts
@@ -10,7 +10,7 @@ const postprocessPlugin: Plugin = {
 // @name:ko        vim comic viewer
 // @description    Universal comic reader
 // @description:ko 만화 뷰어 라이브러리
-// @version        7.0.1
+// @version        7.0.2
 // @namespace      https://greasyfork.org/en/users/713014-nanikit
 // @exclude        *
 // @match          http://unused-field.space/
