80fd2c6df60cec64cca8cf640f7f4adc716705a8
nanikit <nanikit4@gmail.com>
2023-09-09 23:43:55 +0900

    build: change system

--- src/mod.tsx
+++ src/mod.tsx
@@ -1,3 +1,23 @@
+// ==UserScript==
+// @name           vim comic viewer
+// @name:ko        vim comic viewer
+// @description    Universal comic reader
+// @description:ko 만화 뷰어 라이브러리
+// @version        9.0.2
+// @namespace      https://greasyfork.org/en/users/713014-nanikit
+// @exclude        *
+// @match          http://unused-field.space/
+// @author         nanikit
+// @license        MIT
+// @grant          GM_getValue
+// @grant          GM_setValue
+// @grant          GM_xmlhttpRequest
+// @resource       @stitches/react  https://cdn.jsdelivr.net/npm/@stitches/react@1.2.8/dist/index.cjs
+// @resource       fflate           https://cdn.jsdelivr.net/npm/fflate@0.8.0/lib/browser.cjs
+// @resource       react            https://cdn.jsdelivr.net/npm/react@18.2.0/cjs/react.production.min.js
+// @resource       react-dom        https://cdn.jsdelivr.net/npm/react-dom@18.2.0/cjs/react-dom.production.min.js
+// @resource       scheduler        https://cdn.jsdelivr.net/npm/scheduler@0.23.0/cjs/scheduler.production.min.js
+// ==/UserScript==
 import { Viewer } from "./containers/viewer.tsx";
 import { createRef, render } from "./deps.ts";
 import { ViewerController, ViewerOptions } from "./types.ts";
