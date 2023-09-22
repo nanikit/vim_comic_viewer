229f2d48c6573164056a8c7433995373238d0cd7
nanikit <nanikit4@gmail.com>
2023-09-23 05:30:15 +0900

    refactor: atomize setOptions

--- src/atoms/viewer_atoms.ts
+++ src/atoms/viewer_atoms.ts
@@ -1,6 +1,7 @@
 import { atom } from "jotai";
 import { createPageAtom } from "../hooks/create_page_atom.ts";
 import { makeDownloader } from "../hooks/make_downloader.ts";
+import { PageNavigator } from "../hooks/use_page_navigator.ts";
 import { ViewerOptions } from "../types.ts";
 import { gmValueAtom } from "./helper/gm_value_atom.ts";
 
@@ -20,3 +21,42 @@ type ViewerState =
     downloader: ReturnType<typeof makeDownloader>;
   });
 export const viewerStateAtom = atom<ViewerState>({ options: {}, status: "loading" });
+export const setViewerOptionsAtom = atom(
+  null,
+  async (get, set, options: ViewerOptions, navigator: PageNavigator) => {
+    try {
+      const { source } = options;
+      if (source === get(viewerStateAtom).options.source) {
+        return;
+      }
+
+      if (!source) {
+        set(viewerStateAtom, (state) => ({
+          ...state,
+          status: "complete",
+          pages: [],
+          downloader: makeDownloader([]),
+        }));
+        return;
+      }
+
+      set(viewerStateAtom, (state) => ({ ...state, status: "loading" }));
+      const images = await source();
+
+      if (!Array.isArray(images)) {
+        throw new Error(`Invalid comic source type: ${typeof images}`);
+      }
+
+      set(viewerStateAtom, (state) => ({
+        ...state,
+        status: "complete",
+        pages: images.map((x) => createPageAtom({ source: x, observer: navigator.observer })),
+        downloader: makeDownloader(images),
+      }));
+    } catch (error) {
+      set(viewerStateAtom, (state) => ({ ...state, status: "error" }));
+      console.error(error);
+      throw error;
+    }
+  },
+);
