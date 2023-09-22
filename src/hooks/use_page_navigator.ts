65b4b3803573979a006920c053fd73fcb0fa8eca
nanikit <nanikit4@gmail.com>
2023-09-23 06:55:31 +0900

    refactor: atomize resize observer

--- src/hooks/use_page_navigator.ts
+++ src/hooks/use_page_navigator.ts
@@ -1,34 +1,5 @@
-import { useAtomValue, useSetAtom } from "jotai";
-import {
-  goNextAtom,
-  goPreviousAtom,
-  restoreScrollAtom,
-  scrollElementAtom,
-} from "../atoms/viewer_atoms.ts";
-import { useEffect, useRef, useState } from "../deps.ts";
-
-const useResize = <T, E extends Element>(
-  target: E | null,
-  transformer: (target?: ResizeObserverEntry) => T,
-): T => {
-  const [value, setValue] = useState(() => transformer(undefined));
-  const callbackRef = useRef(transformer);
-  callbackRef.current = transformer;
-
-  useEffect(() => {
-    if (!target) {
-      return;
-    }
-
-    const observer = new ResizeObserver((entries) => {
-      setValue(callbackRef.current(entries[0]));
-    });
-    observer.observe(target);
-    return () => observer.disconnect();
-  }, [target, callbackRef]);
-
-  return value;
-};
+import { useSetAtom } from "jotai";
+import { goNextAtom, goPreviousAtom } from "../atoms/viewer_atoms.ts";
 
 export type PageNavigator = {
   goPrevious: () => void;
@@ -36,9 +7,6 @@ export type PageNavigator = {
 };
 
 export const usePageNavigator = (): PageNavigator => {
-  const div = useAtomValue(scrollElementAtom);
-  useResize(div, useSetAtom(restoreScrollAtom));
-
   return {
     goPrevious: useSetAtom(goPreviousAtom),
     goNext: useSetAtom(goNextAtom),
