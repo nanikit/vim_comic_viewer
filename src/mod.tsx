5205679ff7e44054bb3ed83ff20aec8548dde13b
nanikit <nanikit4@gmail.com>
2023-09-23 01:34:00 +0900

    refactor: separate createStore

--- src/mod.tsx
+++ src/mod.tsx
@@ -24,29 +24,42 @@
 // @resource       react-dom           https://cdn.jsdelivr.net/npm/react-dom@18.2.0/cjs/react-dom.production.min.js
 // @resource       scheduler           https://cdn.jsdelivr.net/npm/scheduler@0.23.0/cjs/scheduler.production.min.js
 // ==/UserScript==
-import { Viewer } from "./containers/viewer.tsx";
-import { createRef, render } from "./deps.ts";
+import { createStore, Provider } from "jotai";
+import { InnerViewer } from "./containers/viewer.tsx";
+import { createRef, forwardRef, Ref, render, useMemo } from "./deps.ts";
 import { ViewerController, ViewerOptions } from "./types.ts";
-export { Viewer } from "./containers/viewer.tsx";
 export { download } from "./services/downloader.ts";
 export { transformToBlobUrl } from "./services/user_utils.ts";
 export * as types from "./types.ts";
 export * as utils from "./utils.ts";
 
-const getDefaultRoot = () => {
-  const div = document.createElement("div");
-  div.setAttribute(
-    "style",
-    "width: 0; height: 0; position: fixed;",
+export function initialize(options: ViewerOptions): Promise<ViewerController> {
+  const store = createStore();
+  const ref = createRef<ViewerController>();
+  render(
+    <Provider store={store}>
+      <InnerViewer ref={ref} options={options} useDefault />
+    </Provider>,
+    getDefaultRoot(),
   );
+  return Promise.resolve(ref.current!);
+}
+
+export const Viewer = forwardRef(({ options, useDefault }: {
+  options: ViewerOptions;
+  useDefault?: boolean;
+}, ref: Ref<ViewerController>) => {
+  const store = useMemo(createStore, []);
+  return (
+    <Provider store={store}>
+      <InnerViewer {...{ options, ref, useDefault }} />
+    </Provider>
+  );
+});
+
+function getDefaultRoot() {
+  const div = document.createElement("div");
+  div.setAttribute("style", "width: 0; height: 0; position: fixed;");
   document.body.append(div);
   return div;
-};
-
-export const initialize = (
-  options: ViewerOptions,
-): Promise<ViewerController> => {
-  const ref = createRef<ViewerController>();
-  render(<Viewer ref={ref} options={options} />, getDefaultRoot());
-  return Promise.resolve(ref.current!);
-};
+}
