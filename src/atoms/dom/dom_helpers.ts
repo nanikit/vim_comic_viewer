30d091ef0815ba34071da990f0f948719613f07c
nanikit <nanikit4@gmail.com>
2023-10-31 02:01:42 +0900

    refactor: separate dom mutation

---
+++ src/atoms/dom/dom_helpers.ts
@@ -0,0 +1,20 @@
+const globalCss = document.createElement("style");
+globalCss.innerHTML = `html, body {
+  overflow: hidden;
+}`;
+
+export function showBodyScrollbar(doShow: boolean) {
+  if (doShow) {
+    globalCss.remove();
+  } else {
+    document.head.append(globalCss);
+  }
+}
+
+export async function setFullscreenElement(element: Element | null) {
+  if (element) {
+    await element.requestFullscreen?.();
+  } else {
+    await document.exitFullscreen?.();
+  }
+}
