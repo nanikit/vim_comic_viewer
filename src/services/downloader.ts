d7d7be8c14b3534563904a40525517edb440ee30
nanikit <nanikit4@gmail.com>
2024-02-17 07:01:05 +0900

    refactor: prefer named function

--- src/services/downloader.ts
+++ src/services/downloader.ts
@@ -1,6 +1,6 @@
 import { deferred, zip } from "../deps.ts";
 import { ImageSource } from "../types.ts";
-import { fetchBlob } from "./gm_fetch.ts";
+import { gmFetch, isGmFetchAvailable } from "./gm_fetch.ts";
 import { imageSourceToIterable } from "./image_source_to_iterable.ts";
 
 export type DownloadStatus = "ongoing" | "complete" | "error" | "cancelled";
@@ -18,62 +18,7 @@ export type DownloadOptions = {
   signal?: AbortSignal;
 };
 
-const isGmCancelled = (error: unknown) => {
-  return error instanceof Function;
-};
-
-async function* downloadImage(
-  { source, signal }: {
-    source: ImageSource;
-    signal?: AbortSignal;
-  },
-): AsyncGenerator<{ error: unknown } | { url: string; blob: Blob }> {
-  for await (const url of imageSourceToIterable(source)) {
-    if (signal?.aborted) {
-      break;
-    }
-
-    try {
-      const blob = await fetchBlob(url, { signal });
-      yield { url, blob };
-    } catch (error) {
-      if (isGmCancelled(error)) {
-        yield { error: new Error("download aborted") };
-      } else {
-        yield { error };
-      }
-    }
-  }
-}
-
-const getExtension = (url: string): string => {
-  if (!url) {
-    return ".txt";
-  }
-  const extension = url.match(/\.[^/?#]{3,4}?(?=[?#]|$)/);
-  return extension?.[0] || ".jpg";
-};
-
-const guessExtension = (array: Uint8Array): string | undefined => {
-  const { 0: a, 1: b, 2: c, 3: d } = array;
-  if (a === 0xff && b === 0xd8 && c === 0xff) {
-    return ".jpg";
-  }
-  if (a === 0x89 && b === 0x50 && c === 0x4e && d === 0x47) {
-    return ".png";
-  }
-  if (a === 0x52 && b === 0x49 && c === 0x46 && d === 0x46) {
-    return ".webp";
-  }
-  if (a === 0x47 && b === 0x49 && c === 0x46 && d === 0x38) {
-    return ".gif";
-  }
-};
-
-export const download = (
-  images: ImageSource[],
-  options?: DownloadOptions,
-): Promise<Uint8Array> => {
+export function download(images: ImageSource[], options?: DownloadOptions): Promise<Uint8Array> {
   const { onError, onProgress, signal } = options || {};
   let startedCount = 0;
   let resolvedCount = 0;
@@ -170,4 +115,86 @@ export const download = (
   };
 
   return archiveWithReport(images);
-};
+}
+
+function getExtension(url: string): string {
+  if (!url) {
+    return ".txt";
+  }
+  const extension = url.match(/\.[^/?#]{3,4}?(?=[?#]|$)/);
+  return extension?.[0] || ".jpg";
+}
+
+function guessExtension(array: Uint8Array): string | undefined {
+  const { 0: a, 1: b, 2: c, 3: d } = array;
+  if (a === 0xff && b === 0xd8 && c === 0xff) {
+    return ".jpg";
+  }
+  if (a === 0x89 && b === 0x50 && c === 0x4e && d === 0x47) {
+    return ".png";
+  }
+  if (a === 0x52 && b === 0x49 && c === 0x46 && d === 0x46) {
+    return ".webp";
+  }
+  if (a === 0x47 && b === 0x49 && c === 0x46 && d === 0x38) {
+    return ".gif";
+  }
+}
+
+async function* downloadImage(
+  { source, signal }: {
+    source: ImageSource;
+    signal?: AbortSignal;
+  },
+): AsyncGenerator<{ error: unknown } | { url: string; blob: Blob }> {
+  for await (const url of imageSourceToIterable(source)) {
+    if (signal?.aborted) {
+      break;
+    }
+
+    try {
+      const blob = await fetchBlobWithCacheIfPossible(url, signal);
+      yield { url, blob };
+    } catch (error) {
+      yield await fetchBlobIgnoringCors(url, { signal, fetchError: error });
+    }
+  }
+}
+
+async function fetchBlobWithCacheIfPossible(url: string, signal?: AbortSignal) {
+  const response = await fetch(url, { signal });
+  return await response.blob();
+}
+
+async function fetchBlobIgnoringCors(
+  url: string,
+  { signal, fetchError }: { signal?: AbortSignal; fetchError?: unknown },
+) {
+  if (isCrossOrigin(url) && !isGmFetchAvailable) {
+    return {
+      error: new Error(
+        "It could be a CORS issue but cannot use GM_xmlhttpRequest",
+        { cause: fetchError },
+      ),
+    };
+  }
+
+  try {
+    const blob = await gmFetch(url, { signal }).blob();
+    return { url, blob };
+  } catch (error) {
+    if (isGmCancelled(error)) {
+      return { error: new Error("download aborted") };
+    } else {
+      return { error: fetchError };
+    }
+  }
+}
+
+function isCrossOrigin(url: string) {
+  return new URL(url).origin !== location.origin;
+}
+
+function isGmCancelled(error: unknown) {
+  return error instanceof Function;
+}
