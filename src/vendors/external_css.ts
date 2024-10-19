2cc8a7f8bb5d348a5806158f5dcf6de9dd0ec43c
nanikit <nanikit4@gmail.com>
2024-10-20 05:50:34 +0900

    refactor: remove vendors directory

--- src/vendors/external_css.ts
+++
@@ -1,9 +0,0 @@
-import { insertCss } from "../utils.ts";
-
-insertVendorCss();
-
-async function insertVendorCss() {
-  await Promise.all([
-    GM.getResourceText("overlayscrollbars-css").then(insertCss),
-  ]);
-}
