b90d8bda1425ed62af30a63315fa663628b86b70
nanikit <nanikit4@gmail.com>
2024-12-03 03:30:12 +0900

    feat: handle wheel as page navigation

--- src/atoms/set_scroll_element_atom.ts
+++ src/atoms/set_scroll_element_atom.ts
@@ -1,5 +1,10 @@
 import { atom } from "../deps.ts";
-import { correctScrollAtom, scrollElementStateAtom } from "../features/navigation/atoms.ts";
+import {
+  correctScrollAtom,
+  goNextAtom,
+  goPreviousAtom,
+  scrollElementStateAtom,
+} from "../features/navigation/atoms.ts";
 import {
   isFullscreenPreferredPromiseAtom,
   wasImmersiveAtom,
@@ -30,8 +35,27 @@ export const setScrollElementAtom = atom(null, async (get, set, div: HTMLDivElem
   resizeObserver.observe(div);
   resizeObserver.observe(div.firstElementChild!);
 
+  div.addEventListener("wheel", navigateWithWheel);
+
+  function navigateWithWheel(event: WheelEvent) {
+    const unit = event.deltaMode === WheelEvent.DOM_DELTA_PIXEL ? 10 : 1;
+    const diff = event.deltaY / unit;
+    if (diff >= 1) {
+      set(goNextAtom);
+    } else if (diff <= -1) {
+      set(goPreviousAtom);
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
   set(scrollElementStateAtom, { div, resizeObserver });
   setScrollElementSize();
   await get(isFullscreenPreferredPromiseAtom);
   await set(setViewerImmersiveAtom, get(wasImmersiveAtom));
+
+  return () => {
+    div.removeEventListener("wheel", navigateWithWheel);
+  };
 });
